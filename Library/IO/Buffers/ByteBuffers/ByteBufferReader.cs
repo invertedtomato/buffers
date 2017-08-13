789323edc494b8d516b332a65d277e8b08df49d0
Ben Thompson <ben@invertedtomato.com>
2017-08-13 21:45:29 +1000

    Added initial implimentation of ByteBufferReader and ByteBufferWriter. Needs fleshing out and unit tests.

---
+++ Library/IO/Buffers/ByteBuffers/ByteBufferReader.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace InvertedTomato.IO.Buffers.ByteBuffers {
+    public class ByteBufferReader {
+        private Buffer<byte> Buffer;
+
+        public ByteBufferReader(Buffer<byte> buffer) {
+#if DEBUG
+            if (null == buffer) {
+                throw new ArgumentNullException("buffer");
+            }
+#endif
+            Buffer = buffer;
+        }
+
+
+        public byte ReadUInt8() {
+            return Buffer.Dequeue();
+        }
+
+        public sbyte ReadSInt8() {
+            return (sbyte)Buffer.Dequeue();
+        }
+
+        public ushort ReadUInt16() {
+            var ret = Buffer.DequeueBuffer(2);
+            return BitConverter.ToUInt16(ret.GetUnderlying(), ret.Start);
+        }
+
+        public short ReadSInt16() {
+            var ret = Buffer.DequeueBuffer(2);
+            return BitConverter.ToInt16(ret.GetUnderlying(), ret.Start);
+        }
+
+        public uint ReadUInt32() {
+            var ret = Buffer.DequeueBuffer(4);
+            return BitConverter.ToUInt32(ret.GetUnderlying(), ret.Start);
+        }
+
+        public int ReadSInt32() {
+            var ret = Buffer.DequeueBuffer(4);
+            return BitConverter.ToInt32(ret.GetUnderlying(), ret.Start);
+        }
+
+        public ulong ReadUInt64() {
+            var ret = Buffer.DequeueBuffer(8);
+            return BitConverter.ToUInt64(ret.GetUnderlying(), ret.Start);
+        }
+
+        public long ReadSInt64() {
+            var ret = Buffer.DequeueBuffer(8);
+            return BitConverter.ToInt64(ret.GetUnderlying(), ret.Start);
+        }
+
+        public float ReadFloat() {
+            var ret = Buffer.DequeueBuffer(4);
+            return BitConverter.ToSingle(ret.GetUnderlying(), ret.Start);
+        }
+
+        public double ReadDouble() {
+            var ret = Buffer.DequeueBuffer(8);
+            return BitConverter.ToDouble(ret.GetUnderlying(), ret.Start);
+        }
+
+        public bool ReadBoolean() {
+            return Buffer.Dequeue() > 0;
+        }
+
+        public Guid ReadGuid() {
+            return new Guid(Buffer.DequeueArray(16));
+        }
+
+        public byte[] Read(int length) {
+#if DEBUG
+            if (length < 0) {
+                throw new ArgumentOutOfRangeException("length");
+            }
+#endif
+
+            return Buffer.DequeueArray(length);
+        }
+    }
+}
---
+++ Library/IO/Buffers/ByteBuffers/ByteBufferWriter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace InvertedTomato.IO.Buffers.ByteBuffers {
+    public class ByteBufferWriter {
+        private const int GrowthRate = 2;
+
+        private Buffer<byte> Buffer;
+
+        public ByteBufferWriter(int initialCapacity) {
+#if DEBUG
+            if (initialCapacity < 1) {
+                throw new ArgumentOutOfRangeException("Must be at least 1 byte.", "initialCapacity");
+            }
+#endif
+
+            Buffer = new Buffer<byte>(initialCapacity);
+        }
+
+        public ByteBufferWriter WriteUInt8(byte value) {
+            return Write(new byte[] { value });
+        }
+
+        public ByteBufferWriter WriteSInt8(sbyte value) {
+            return Write(new byte[] { (byte)value });
+        }
+
+        public ByteBufferWriter WriteUInt16(ushort value) {
+            return Write(BitConverter.GetBytes(value));
+        }
+
+        public ByteBufferWriter WriteSInt16(short value) {
+            return Write(BitConverter.GetBytes(value));
+        }
+
+        public ByteBufferWriter WriteUInt32(uint value) {
+            return Write(BitConverter.GetBytes(value));
+        }
+
+        public ByteBufferWriter WriteSInt32(int value) {
+            return Write(BitConverter.GetBytes(value));
+        }
+
+        public ByteBufferWriter WriteUInt64(ulong value) {
+            return Write(BitConverter.GetBytes(value));
+        }
+
+        public ByteBufferWriter WriteSInt64(long value) {
+            return Write(BitConverter.GetBytes(value));
+        }
+
+        public ByteBufferWriter WriteFloat(float value) {
+            return Write(BitConverter.GetBytes(value));
+        }
+
+        public ByteBufferWriter WriteDouble(double value) {
+            return Write(BitConverter.GetBytes(value));
+        }
+
+        public ByteBufferWriter WriteBoolean(bool value, byte trueValue = 0xff) {
+#if DEBUG
+            if (trueValue == 0) {
+                throw new ArgumentOutOfRangeException("Cannot be equal to 0.", "trueValue");
+            }
+#endif
+            return Write(new byte[] { value ? trueValue : (byte)0x00 });
+        }
+
+        public ByteBufferWriter WriteGuid(Guid value) {
+            return Write(value.ToByteArray());
+        }
+
+        public ByteBufferWriter Write(byte[] value) {
+#if DEBUG
+            if (null == value) {
+                throw new ArgumentNullException("value");
+            }
+#endif
+
+            // If buffer would overflow...
+            if (Buffer.Writable < value.Length) {
+                // Increase it's capacity
+                Buffer = Buffer.Resize(Math.Max(Buffer.Capacity + value.Length, Buffer.Capacity * GrowthRate));
+            }
+
+            // Add to queue
+            Buffer.EnqueueArray(value);
+
+            return this;
+        }
+
+        public Buffer<byte> ToByteBuffer() {
+            return Buffer;
+        }
+    }
+}
